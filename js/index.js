2c29a406af887654205b06c6a115f9dfac972998
Shaun Lee <shaundlee.0103@gmail.com>
2020-06-02 17:03:09 -0400

    Added variables for accessing elements

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+const logo = document.querySelector('.logo-heading')
+const navs = document.querySelectorAll('.nav-link')
+const imgs = document.querySelectorAll('img')
+const contentTxt = document.querySelectorAll('p')
+const headings = document.querySelectorAll('h2')
+const vacationBox = document.querySelector('.content-pick')
+const secondButton = document.querySelectorAll('.btn')[1]
+const footer = document.querySelector('footer')
+
