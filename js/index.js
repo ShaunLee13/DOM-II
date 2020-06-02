75d9a472c73004b9eccedbef5c6e07a9a4f7b739
Shaun Lee <shaundlee.0103@gmail.com>
2020-06-02 17:49:06 -0400

    Added click effect to nav, and preventdefault

--- js/index.js
+++ js/index.js
@@ -9,17 +9,28 @@ const vacationBox = document.querySelector('.content-pick')
 const secondButton = document.querySelectorAll('.btn')[1]
 const footer = document.querySelector('footer')
 
-//Functions for Logo effects
 
+//Functions for Logo effects
 function logoWheel(event){
     logo.style.color = 'pink'
+    setTimeout(function() {
+        logo.style.color = "";
+      }, 250);
 }
-
 document.addEventListener('wheel', logoWheel)
+//End of Logo effects
 
-
-
-
-
-
-
+//Nav Effects
+navs.forEach((nav) => {
+    nav.addEventListener('click', () => {
+        nav.style.color = 'red';
+        setTimeout(function() {
+            nav.style.color = "firebrick";
+        }, 250)
+        setTimeout(function() {
+            nav.style.color = "";
+        }, 250)
+        event.preventDefault()
+    })
+})
+//End of Nav effects
